ccfce162bd1fe8c4d5307d13808d395ecb0a9e74
Anurag Sahu <sahuanuragdev@gmail.com>
2024-06-24 19:01:35 +0530

    [chore] Changed page size

--- src/views/product-listing-page/ProductListingPage.jsx
+++ src/views/product-listing-page/ProductListingPage.jsx
@@ -5,7 +5,7 @@ import { getProductsData } from "../../utils/product-listing-utils";
 import { useInfiniteScroll } from "../../hooks/useInfiniteScroll";
 import { ProductListingCard } from "../../components/general-components/cards/product-listing-card/ProductListingCard";
 import { useEffect } from "react";
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 9;
 
 export const ProductListingPage = () => {
   // * REDUX STORE
@@ -40,14 +40,14 @@ export const ProductListingPage = () => {
 
   return (
     <div className="flex justify-center w-full bg-slate-100">
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  w-full	gap-x-10 gap-y-14 w-fit	 py-16">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3	gap-x-10 gap-y-14 w-fit	 py-16">
         {data?.pages?.map((group) => {
           return group.products.map((cardData) => (
             <ProductListingCard productData={cardData} key={cardData.id} />
           ));
         })}
         {(isLoading || isFetching) &&
-          Array(4)
+          Array(3)
             .fill(0)
             .map((item, index) => <ProductListingCardShimmer key={index} />)}
       </div>
